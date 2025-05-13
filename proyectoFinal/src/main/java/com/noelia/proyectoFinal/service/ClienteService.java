b4aa86cb8b2116daa4efd82a0043c1000180796e
NoeliaCardeaSanchez <noelia-cardenas1@eep-igroup.com>
2025-05-13 11:07:38 +0200

    Servicio de cliente

---
+++ proyectoFinal/src/main/java/com/noelia/proyectoFinal/service/ClienteService.java
@@ -0,0 +1,37 @@
+package com.noelia.proyectoFinal.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.noelia.proyectoFinal.entitys.Cliente;
+import com.noelia.proyectoFinal.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> obtenerTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> obtenerPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente guardar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void eliminar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
