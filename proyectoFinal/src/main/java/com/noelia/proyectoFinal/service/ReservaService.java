742fe2172854667076d1144e86e968187674ecc2
NoeliaCardeaSanchez <noelia-cardenas1@eep-igroup.com>
2025-05-13 11:07:59 +0200

    Servicio de reserva

---
+++ proyectoFinal/src/main/java/com/noelia/proyectoFinal/service/ReservaService.java
@@ -0,0 +1,37 @@
+package com.noelia.proyectoFinal.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.noelia.proyectoFinal.entitys.Reserva;
+import com.noelia.proyectoFinal.repositories.ReservaRepository;
+
+@Service
+public class ReservaService {
+
+    private final ReservaRepository reservaRepository;
+
+    @Autowired
+    public ReservaService(ReservaRepository reservaRepository) {
+        this.reservaRepository = reservaRepository;
+    }
+
+    public List<Reserva> obtenerTodas() {
+        return reservaRepository.findAll();
+    }
+
+    public Optional<Reserva> obtenerPorId(Long id) {
+        return reservaRepository.findById(id);
+    }
+
+    public Reserva guardar(Reserva reserva) {
+        return reservaRepository.save(reserva);
+    }
+
+    public void eliminar(Long id) {
+        reservaRepository.deleteById(id);
+    }
+}
