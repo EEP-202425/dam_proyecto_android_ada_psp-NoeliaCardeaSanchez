71b5ba200f6e13a69d93406edb9f4ecf3a74c631
NoeliaCardeaSanchez <noelia-cardenas1@eep-igroup.com>
2025-05-17 14:00:29 +0200

    Mapeacion campos  de JPA
    PILOTO Y AVION

---
+++ front/app/src/main/java/com/example/proyectofinaland/model/Avion.kt
@@ -0,0 +1,8 @@
+package com.example.proyectofinaland.model
+
+data class Avion(
+    val id: Long,
+    val nombre: String,
+    val modelo: String,
+    val matricula: String
+)
---
+++ front/app/src/main/java/com/example/proyectofinaland/model/Piloto.kt
@@ -0,0 +1,8 @@
+package com.example.proyectofinaland.model
+
+data class Piloto(
+    val id: Long,
+    val nombre: String,
+    val apellidos: String,
+    val experienciaAnios: Int
+)
