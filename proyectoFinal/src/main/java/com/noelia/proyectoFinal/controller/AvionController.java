ecbabc6f2566342cf6d6fcc78cf6a4195e7bf178
NoeliaCardeaSanchez <noelia-cardenas1@eep-igroup.com>
2025-05-19 10:37:17 +0200

    Controller

--- proyectoFinal/src/main/java/com/noelia/proyectoFinal/controller/AvionController.java
+++ proyectoFinal/src/main/java/com/noelia/proyectoFinal/controller/AvionController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.noelia.proyectoFinal.entitys.Avion;
 import com.noelia.proyectoFinal.entitys.Piloto;
-import com.noelia.proyectoFinal.repositories.PilotoRepository;
 import com.noelia.proyectoFinal.service.AvionService;
 import com.noelia.proyectoFinal.service.PilotoService;
 
